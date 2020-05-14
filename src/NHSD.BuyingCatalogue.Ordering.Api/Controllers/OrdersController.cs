b1fcbf2f91a2807e9b68e7684e05d3198fa7c9d2
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-05-14 13:41:13 +0100

    Fix build

--- src/NHSD.BuyingCatalogue.Ordering.Api/Controllers/OrdersController.cs
+++ src/NHSD.BuyingCatalogue.Ordering.Api/Controllers/OrdersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using NHSD.BuyingCatalogue.Ordering.Api.Models;
 using NHSD.BuyingCatalogue.Ordering.Application.Persistence;
 using NHSD.BuyingCatalogue.Ordering.Api.Models.Summary;
+using NHSD.BuyingCatalogue.Ordering.Common.Constants;
 
 namespace NHSD.BuyingCatalogue.Ordering.Api.Controllers
 {
@@ -17,6 +18,33 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.Controllers
     [Authorize(Policy = PolicyName.CanAccessOrders)]
     public sealed class OrdersController : Controller
     {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrdersController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        }
+
+        [HttpGet]
+        [Route("/api/v1/organisations/{organisationId}/[controller]")]
+        public async Task<ActionResult> GetAllAsync(Guid organisationId)
+        {
+            var orders = await _orderRepository.ListOrdersByOrganisationIdAsync(organisationId);
+
+            var orderModelResult = orders.Select(order => new OrderModel()
+                {
+                    OrderId = order.OrderId,
+                    Description = order.Description,
+                    LastUpdatedBy = order.LastUpdatedBy,
+                    LastUpdated = order.LastUpdated,
+                    DateCreated = order.Created,
+                    Status = order.OrderStatus.Name
+                })
+                .ToList();
+
+            return Ok(orderModelResult);
+        }
+
         [HttpGet]
         [Route("{orderId}/summary")]
         public async Task<ActionResult> GetOrderSummaryAsync(string orderId)
