ee44421b67fb1654c71ed0b5aeece42007a47be4
Sam Roberts <53512773+sam-roberts-bjss@users.noreply.github.com>
2020-05-14 13:53:42 +0100

    Add access token

--- tests/NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests/Features/Orders GetAll.feature
+++
@@ -1,8 +0,0 @@
-﻿Feature: Orders GetAll
-	In order to avoid silly mistakes
-	As a math idiot
-	I want to be told the sum of two numbers
-
-@mytag
-Scenario: Call the get all orders endpoint
-	When the user executes the get all orders request
--- tests/NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests/Utils/Request.cs
+++ tests/NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests/Utils/Request.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Flurl;
 using Flurl.Http;
+using IdentityModel.Client;
 using NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps.Common;
 using NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Steps.Support;
 using TechTalk.SpecFlow;
@@ -19,20 +22,67 @@ namespace NHSD.BuyingCatalogue.Ordering.Api.IntegrationTests.Utils
         }
 
         public async Task GetAsync(string url, params object[] pathSegments)
-            => _response.Result = await CreateCommonRequest(url, pathSegments).GetAsync();
+        {
+            string token = await GetAccessTokenAsync();
+            _response.Result = await CreateCommonRequest(url, token, pathSegments).GetAsync();
+        }
 
         public async Task PostJsonAsync(string url, object payload, params object[] pathSegments)
-            => _response.Result = await CreateCommonRequest(url, pathSegments).PostJsonAsync(payload);
+        {
+            string token = await GetAccessTokenAsync();
+            _response.Result = await CreateCommonRequest(url, token, pathSegments).PostJsonAsync(payload);
+        }
 
         public async Task PutJsonAsync(string url, object payload, params object[] pathSegments)
-            => _response.Result = await CreateCommonRequest(url, pathSegments).PutJsonAsync(payload);
+        {
+            string token = await GetAccessTokenAsync();
+            _response.Result = await CreateCommonRequest(url, token, pathSegments).PutJsonAsync(payload);
+        }
 
-        private IFlurlRequest CreateCommonRequest(string url, params object[] pathSegments)
+        private IFlurlRequest CreateCommonRequest(string url, string token, params object[] pathSegments)
         {
             return url
                 .AppendPathSegments(pathSegments)
-                .WithOAuthBearerToken(_context.Get(ScenarioContextKeys.AccessToken, string.Empty))
+                .WithOAuthBearerToken(_context.Get(ScenarioContextKeys.AccessToken, token))
                 .AllowAnyHttpStatus();
+        }
+
+        private async Task<string> GetAccessTokenAsync()
+        {
+            var discoveryAddress = "http://host.docker.internal:8070/identity";
+
+            using var client = new HttpClient();
+
+            var discoveryDocument =
+                await client.GetDiscoveryDocumentAsync(new DiscoveryDocumentRequest
+                {
+                    Policy = new DiscoveryPolicy { RequireHttps = false },
+                    Address = discoveryAddress,
+                });
+
+            if (discoveryDocument.IsError)
+            {
+                Console.WriteLine(discoveryDocument.Error);
+                return string.Empty;
+            }
+
+            TokenResponse tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                Address = discoveryDocument.TokenEndpoint,
+                ClientId = "PasswordClient",
+                ClientSecret = "PasswordSecret",
+                UserName = "alicesmith@email.com",
+                Password = "Pass123$",
+                Scope = "Ordering"
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return string.Empty;
+            }
+
+            return tokenResponse.AccessToken;
         }
     }
 }
